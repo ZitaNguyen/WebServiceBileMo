6c167107e76e6dd73df9a1979871a8b50aa9a6e9
znguyen <znguyen@getraline.com>
2023-10-29 21:39:53 +0100

    Add route to get a single product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -9,9 +10,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
+
 class ProductController extends AbstractController
 {
-    #[Route('/api/products', name: 'product', methods: ['GET'])]
+    #[Route('/api/products', name: 'products', methods: ['GET'])]
     public function getProductList(ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
     {
         $productList = $productRepository->findAll();
@@ -20,4 +22,11 @@ class ProductController extends AbstractController
         return new JsonResponse($jsonProductList, Response::HTTP_OK, [], true);
 
     }
+
+    #[Route('/api/products/{id}', name: 'product', methods: ['GET'])]
+    public function getSingleProduct(Product $product, SerializerInterface $serializer): JsonResponse {
+
+        $jsonProduct = $serializer->serialize($product, 'json');
+        return new JsonResponse($jsonProduct, Response::HTTP_OK, ['accept' => 'json'], true);
+   }
 }
